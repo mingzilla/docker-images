7e9a07f3e729afad619fea7aeba471585df564b6
Ming.Huang <means789@gmail.com>
2026-02-11 10:10:19 +0000

    vllm - nvpf4

---
+++ vllm-qwen3_32b_direct/run_cmd.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+cd "$(dirname "$0")"
+source ../_common/common_cmd.sh
+
+##############################################################
+
+if [ $# -eq 0 ]; then
+    help::show "qwen3"
+    exit 1
+fi
+
+if [ "$1" == "curl_qwen3" ]; then
+    curl::test_model "30202" "nvidia/Qwen3-32B-NVFP4"
+fi
+
+if [ "$1" == "log_qwen3" ]; then
+    docker::view_logs "vllm-qwen3-direct"
+fi
+
+if [ "$1" == "save_qwen3" ]; then
+    docker::save_as_tar "vllm-qwen3-nvfp4:local" "vllm-qwen3-nvfp4.tar"
+fi
